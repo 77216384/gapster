37334ce066674b9674af027ddc7fc1711fa432d6
exchez <alexjsanchez@gmail.com>
2017-10-29 23:54:39 -0700

    cleaned up js

--- app/static/script.js
+++ app/static/script.js
@@ -42,115 +42,11 @@ $('.choiceButton').click(function(e){
     $(e.target).addClass('btn-success').removeClass('btn-outline-light');
     var buttons = $('.choiceButton');
     for (i = 0; i < buttons.length; i++) { 
-    if ($(buttons[i]).text() != answer) {
-      $(buttons[i]).fadeOut('slow');
+      if ($(buttons[i]).text() != answer) {
+        $(buttons[i]).fadeOut('slow');
+      }
     }
-}
   } else {
     $(e.target).addClass('btn-danger').removeClass('btn-outline-light');
   }
-});
-
-$('#buddhism').click(function(e){
-  e.preventDefault();
-  $('#questionLoading').show();
-  $.ajax({
-    type: "GET",
-    url: "/fetch_article/buddhism",
-    success: function(data){
-      $.ajax({
-        type: "POST",
-        url: "/question",
-        data: JSON.stringify(data),
-        contentType: "application/json; charset=utf-8",
-        success: function(data){
-          $('#questionLoading').hide();
-          $('#questionText').text(data.question);
-          $('#choiceOne').text(data.answer);
-          $('#choiceTwo').text(data.distractors[0]);
-          $('#choiceThree').text(data.distractors[1]);
-          $('#choiceFour').text(data.distractors[2]);
-        },
-        dataType: 'json'
-      });
-    }
-  });
-});
-
-$('#lincoln').click(function(e){
-  e.preventDefault();
-  $('#questionLoading').show();
-  $.ajax({
-    type: "GET",
-    url: "/fetch_article/lincoln",
-    success: function(data){
-      $.ajax({
-        type: "POST",
-        url: "/question",
-        data: JSON.stringify(data),
-        contentType: "application/json; charset=utf-8",
-        success: function(data){
-          $('#questionLoading').hide();
-          $('#questionText').text(data.question);
-          $('#choiceOne').text(data.answer);
-          $('#choiceTwo').text(data.distractors[0]);
-          $('#choiceThree').text(data.distractors[1]);
-          $('#choiceFour').text(data.distractors[2]);
-        },
-        dataType: 'json'
-      });
-    }
-  });
-});
-
-$('#snoop').click(function(e){
-  e.preventDefault();
-  $('#questionLoading').show();
-  $.ajax({
-    type: "GET",
-    url: "/fetch_article/snoop",
-    success: function(data){
-      $.ajax({
-        type: "POST",
-        url: "/question",
-        data: JSON.stringify(data),
-        contentType: "application/json; charset=utf-8",
-        success: function(data){
-          $('#questionLoading').hide();
-          $('#questionText').text(data.question);
-          $('#choiceOne').text(data.answer);
-          $('#choiceTwo').text(data.distractors[0]);
-          $('#choiceThree').text(data.distractors[1]);
-          $('#choiceFour').text(data.distractors[2]);
-        },
-        dataType: 'json'
-      });
-    }
-  });
-});
-
-$('#wormhole').click(function(e){
-  e.preventDefault();
-  $('#questionLoading').show();
-  $.ajax({
-    type: "GET",
-    url: "/fetch_article/wormhole",
-    success: function(data){
-      $.ajax({
-        type: "POST",
-        url: "/question",
-        data: JSON.stringify(data),
-        contentType: "application/json; charset=utf-8",
-        success: function(data){
-          $('#questionLoading').hide();
-          $('#questionText').text(data.question);
-          $('#choiceOne').text(data.answer);
-          $('#choiceTwo').text(data.distractors[0]);
-          $('#choiceThree').text(data.distractors[1]);
-          $('#choiceFour').text(data.distractors[2]);
-        },
-        dataType: 'json'
-      });
-    }
-  });
 });
