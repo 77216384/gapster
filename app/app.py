5065afc363842657ccdda4d5e96ad968eb5b155d
exchez <alexjsanchez@gmail.com>
2017-11-01 16:20:38 -0700

    making one big blank

--- app/app.py
+++ app/app.py
@@ -67,6 +67,7 @@ def make_question():
 		question = best_questions[0]['question']
 		answer = best_questions[0]['answer']
 		distractors = DistractorSet(best_questions[0], text, spacy_text, nlp).distractors
+		qestion['question'] = re.sub('(_\s)+', '___________ ', question['question'])
 		output = {'question': question, 'answer': answer.lower(), 'distractors': distractors}
 
 		return jsonify(output)
